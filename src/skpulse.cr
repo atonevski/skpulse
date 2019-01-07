e9fe4b412a5f2a0f77faad6f52d4ff128bbacfcc
Andreja Tonevski <atonevski@gmail.com>
2019-01-07 19:12:22 +0100

    1st commit, complete functional api

---
+++ src/skpulse.cr
@@ -0,0 +1,191 @@
+require "http/client"
+require "uri"
+require "json"
+require "base64"
+
+# TODO: Write documentation for `Skpulse`
+module SKPulse
+  VERSION = "0.1.0"
+ 
+  class API
+    USERNAME = "atonevski"
+    PASSWORD = "pv1530kay" 
+    CITY = "skopje"
+    TM_FMT = "%Y-%m-%dT%H:%M:%S%:z"
+
+    getter sensors : JSON::Any
+    getter measurements : JSON::Any
+
+    def initialize(@username = USERNAME, @password = PASSWORD)
+      @sensors = JSON.parse "[]"
+      @measurements = JSON.parse "[]"
+      @hostname = "#{ CITY }.pulse.eco"
+      @enc64 = Base64.strict_encode @username + ":" + @password
+    end
+
+    # you should call immediately after .new
+    def get_sensors
+      HTTP::Client.new host: @hostname, port: 443, tls: true do |client|
+        path = "/rest/sensor"
+        headers = HTTP::Headers {
+          "Accept-Charset" => "utf-8",
+          "Accept" => "application/json",
+          "Authorization" => "Basic #{ @enc64 }"
+        }
+        client.get path, headers: headers do |res|
+          @sensors = JSON.parse res.body_io.gets_to_end
+        end
+      end
+      @sensors
+    end
+
+    # get last 24h measurements from all sensors for all parameters
+    def get_24h
+      HTTP::Client.new host: @hostname, port: 443, tls: true do |client|
+        path = "/rest/data24h"
+        headers = HTTP::Headers {
+          "Accept-Charset" => "utf-8",
+          "Accept" => "application/json",
+          "Authorization" => "Basic #{ @enc64 }"
+        }
+        client.get path, headers: headers do |res|
+          @measurements = JSON.parse res.body_io.gets_to_end
+        end
+      end
+    end
+
+    # get rawdata 
+    def get_raw_data(sensor_id : String, value_type : String, from : Time, to : Time)
+      HTTP::Client.new host: @hostname, port: 443, tls: true do |client|
+        path = "/rest/dataRaw?sensorId=#{ sensor_id }&" +
+               "type=#{ value_type.downcase }&" +
+               "from=#{ URI.escape from.to_s TM_FMT }&" +
+               "to=#{ URI.escape to.to_s TM_FMT }"
+        headers = HTTP::Headers {
+          "Accept-Charset" => "utf-8",
+          "Accept" => "application/json",
+          "Authorization" => "Basic #{ @enc64 }"
+        }
+        begin
+          client.get path, headers: headers do |res|
+            @measurements = JSON.parse res.body_io.gets_to_end
+          end
+        rescue ex
+          puts "Get RawData exception: #{ ex.message }"
+        end
+      end
+    end
+
+    # get rawdata 
+    def get_raw_data_by_sensor(sensor_id : String, from : Time, to : Time)
+      HTTP::Client.new host: @hostname, port: 443, tls: true do |client|
+        path = "/rest/dataRaw?sensorId=#{ sensor_id }&" +
+               "from=#{ URI.escape from.to_s TM_FMT }&" +
+               "to=#{ URI.escape to.to_s TM_FMT }"
+        headers = HTTP::Headers {
+          "Accept-Charset" => "utf-8",
+          "Accept" => "application/json",
+          "Authorization" => "Basic #{ @enc64 }"
+        }
+        begin
+          client.get path, headers: headers do |res|
+            @measurements = JSON.parse res.body_io.gets_to_end
+          end
+        rescue ex
+          puts "Get RawData exception: #{ ex.message }"
+        end
+      end
+    end
+
+    # get rawdata 
+    def get_raw_data_by_type(value_type : String, from : Time, to : Time)
+      HTTP::Client.new host: @hostname, port: 443, tls: true do |client|
+        path = "/rest/dataRaw?" +
+               "type=#{ value_type.downcase }&" +
+               "from=#{ URI.escape from.to_s TM_FMT }&" +
+               "to=#{ URI.escape to.to_s TM_FMT }"
+        headers = HTTP::Headers {
+          "Accept-Charset" => "utf-8",
+          "Accept" => "application/json",
+          "Authorization" => "Basic #{ @enc64 }"
+        }
+        begin
+          client.get path, headers: headers do |res|
+            @measurements = JSON.parse res.body_io.gets_to_end
+          end
+        rescue ex
+          puts "Get RawData exception: #{ ex.message }"
+        end
+      end
+    end
+
+    def print_sensor(sensor_id : String)
+      sensor = @sensors.as_a.select {|s| s["sensorId"] == sensor_id}[0]
+
+      arr = @measurements.as_a.select {|x| x["sensorId"] == sensor_id}
+      tms = arr.map {|x| x["stamp"].as_s}.uniq
+      types = arr.map {|x| x["type"].as_s}.uniq
+
+      type_index = {} of String => Int32 # Hash(String, Int32)
+      types.each_with_index do |t, i|
+        type_index[t] = i
+      end
+
+      h = {} of String  => Array(String|Nil)
+      arr.each do |m|
+        unless h.has_key? m["stamp"]
+          h[m["stamp"].as_s] = Array(String|Nil).new(types.size, nil)
+        end
+        h[m["stamp"].as_s][type_index[m["type"]]] = m["value"].as_s
+      end
+
+      puts "Sensor: #{ sensor["description"] }"
+      puts "Id: #{ sensor["sensorId"] }"
+      puts "Status: #{ sensor["status"] }"
+      puts 
+      printf "%-19s", "time"
+      types.each {|t| printf " %11.11s", t}
+      puts
+      tms.each do |t|
+        printf "%-19.19s", t
+        h[t].each do |m|
+          printf " %11.11s", (m.nil? ? "" : m)
+        end
+        puts
+      end
+    end
+  end
+  
+  skp_api = API.new
+  skp_api.get_sensors
+
+  sensor_id = skp_api.sensors[1]["sensorId"].as_s
+  skp_api.get_24h
+  
+  skp_api.print_sensor sensor_id
+
+  # # TODO:
+  # - levels: borrow from air.cr
+  # - CLI interface
+  #    [flags] commands...
+  #    1. 24h: sorted by sensor
+  #    2. sensor: one sensor all parameters...
+  # 
+  # # DONE:
+  # - finish all get_raw_data variants
+
+end
+
+# PM10 aq
+# 
+# [
+#   {'from':0.0,'to':15.0,'legendPoint':0.0,'legendColor':'green',
+#   'markerColor':'green','shortGrade':'Good air quality.','grade':'Good air quality. Air quality is considered satisfactory, and air pollution poses little or no risk','suggestion':'No preventive measures needed, enjoy the fresh air.'},
+#   {'from':16.0,'to':30.0,'legendPoint':23.0,'legendColor':'darkgreen','markerColor':'darkgreen','shortGrade':'Moderate air quality.','grade':'Moderate air quality. Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people.','suggestion':'Consider limiting your outside exposure if you\'re sensitive to bad air.'},
+#   {'from':31.0,'to':55.0,'legendPoint':45.0,'legendColor':'orange','markerColor':'orange','shortGrade':'Bad air quality.','grade':'Bad air quality. Unhealthy for Sensitive Groups, people with lung disease, older adults and children.','suggestion':'Limit your outside exposure if you\'re sensitive to bad air.'},
+#   {'from':56.0,'to':110.0,'legendPoint':83.0,'legendColor':'red','markerColor':'red','shortGrade':'Very bad air quality.','grade':'Very bad air quality. Everyone may begin to experience some adverse health effects, and members of the sensitive groups may experience more serious effects.','suggestion':'Stay indoors if you\'re sensitive to bad air. Everyone should limit outside exposure'},
+#   {'from':111.0,'to':1000.0,'legendPoint':110.0,'legendColor':'darkred','markerColor':'darkred','shortGrade':'Hazardous air quality!','grade':'Hazardous air quality! This would trigger a health warnings of emergency conditions. The entire population is more likely to be affected!','suggestion':'Stay indoors as much as possible.'}]
+
+
+# PM25
+#[{'from':0.0,'to':20.0,'legendPoint':0.0,'legendColor':'green','markerColor':'green','shortGrade':'Overall silence.','grade':'Overall silence. Human hearing can barely register noticeable noise.','suggestion':'Enjoy the peace and quiet.'},{'from':21.0,'to':40.0,'legendPoint':30.0,'legendColor':'darkgreen','markerColor':'darkgreen','shortGrade':'Generally quiet.','grade':'Generally quiet. Noise levels are noticeable but cause no discomfort.','suggestion':'No precautions necessary, noise levels are low.'},{'from':41.0,'to':60.0,'legendPoint':50.0,'legendColor':'orange','markerColor':'orange','shortGrade':'Moderately calm, about the level of a conversation.','grade':'Moderately calm, about the level of a conversation. Shouldn\'t cause discomfort','suggestion':'Try to limit exposure if you\'re very sensitive to noise.'},{'from':61.0,'to':85.0,'legendPoint':68.0,'legendColor':'red','markerColor':'red','shortGrade':'Noisy urban daytime.','grade':'Noisy urban daytime. Standard city noise pollution, can cause discomfort.','suggestion':'Try to avoid prolonged exposure as it can be irritating, no medical precautions necessary.'},{'from':86.0,'to':140.0,'legendPoint':88.0,'legendColor':'darkred','markerColor':'darkred','shortGrade':'Generally loud. Will likely cause discomfort.','grade':'Generally loud. Will likely cause discomfort. Temporary hearing loss possible after prolonged exposure.','suggestion':'Try to stay indoors. If you\'re experiencing any ear ringing, rest your ears for 16 hours to recover.'},{'from':141.0,'to':255.0,'legendPoint':151.0,'legendColor':'purple','markerColor':'purple','shortGrade':'Uncomfortably loud.','grade':'Uncomfortably loud. Temporary hearing loss may occur after prolonged exposure with possible ear pain and dizziness.','suggestion':'Stay indoors as much as possible. Consider wearing ear protection if you\'re going outside for longer periods.'}]
